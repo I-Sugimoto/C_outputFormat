57b44cfc597c57aa4ebc06547ce2a73c106df8e8
Ai Sugimoto <i.sugimoto1112@gmail.com>
2015-11-07 16:12:54 +0900

    modified-1

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 $m = 71;
-$n =  4;
+$n = 5;
 
-$val = str_pad($m, $n, 0, STR_PAD_LEFT);
-
-echo $var;
-
+if(($m <=0) && ($n < 2))
+{
+    echo "mは、正の整数を入力してください。". "<br>";
+    echo "nは、2以上の数を入力してください。". "<br>";
+} 
+elseif($m <= 0) 
+{
+	echo "mは、正の整数を入力してください。";
+}
+elseif($n < 2) 
+{
+	echo "nは、2以上の数を入力してください。";
+}
+else
+{
+	$val = str_pad($m, $n, 0, STR_PAD_LEFT);
+	echo $val;
+}
